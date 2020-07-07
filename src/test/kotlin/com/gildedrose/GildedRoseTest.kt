695f2f1a663536bf93fb8e6e3cfd779419c2e739
Vincent Viane <vincentviane@MacBook-Pro-de-Vincent.local>
2020-07-07 17:00:50 +0200

    Initial commit

---
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -0,0 +1,52 @@
+package com.gildedrose
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class GildedRoseTest {
+
+    /*@Test fun foo() {
+        val items = arrayOf<Item>(Item("foo", 0, 0))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("fixme", app.items[0].name)
+
+    }*/
+
+    private fun updateItems(items: Array<Item>)
+    {
+        val app = GildedRose(items)
+        app.updateQuality()
+    }
+
+    private val items = arrayOf(Item("+5 Dexterity Vest", 10, 20), //
+    Item("Aged Brie", 2, 0), //
+    Item("Elixir of the Mongoose", 5, 7), //
+    Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+    Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    // this conjured item does not work properly yet
+    Item("Conjured Mana Cake", 3, 6))
+
+    @Test fun normalUpdate() {
+        var expectedItems = arrayOf(Item("+5 Dexterity Vest",9,19))
+
+        val items = arrayOf(Item("+5 Dexterity Vest",10,20))
+
+        updateItems(items)
+        assertEquals(items.contentToString(), expectedItems.contentToString())
+    }
+
+    @Test fun sulfurasUpdate() {
+
+        val items = arrayOf(Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                Item("Sulfuras, Hand of Ragnaros", -1, 80))
+
+        updateItems(items)
+        assertEquals(items.contentToString(), items.contentToString())
+    }
+}
+
+
